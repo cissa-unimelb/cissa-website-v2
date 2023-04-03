ef9874d87a2e844b3e2679b432b2e79f747878f5
khangluong2004 <khangluong2004@gmail.com>
2023-04-03 21:42:57 +1000

    Mimic original page

---
+++ src/heroPanel.jsx
@@ -0,0 +1,18 @@
+import './assets/css/hero.css'
+import logo from './assets/images/logo_transparent.png'
+
+function HeroPanel(props) {
+  return (
+    <div id='banner'>
+      <div id='banner-overlay'>
+        <img src={logo} id='banner-logo' />
+        <div id='tagline-container'>
+          <span id='tagline'>UniMelb's Premier Tech Society</span>
+          <span class='blinking-cursor'>_</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroPanel
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import HeroPanel from './heroPanel'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  </React.StrictMode>
+    <HeroPanel />
+  </React.StrictMode>,
 )
