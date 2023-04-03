7a19597b9fdb7f513bab90d9774d0c4c9eed8b5d
khangluong2004 <khangluong2004@gmail.com>
2023-04-03 22:39:15 +1000

    Vertical align the logo

--- src/heroPanel.jsx
+++ src/heroPanel.jsx
@@ -5,10 +5,12 @@ function HeroPanel(props) {
   return (
     <div id='banner'>
       <div id='banner-overlay'>
-        <img src={logo} id='banner-logo' />
-        <div id='tagline-container'>
-          <span id='tagline'>UniMelb's Premier Tech Society</span>
-          <span class='blinking-cursor'>_</span>
+        <div id='centre-fix'>
+            <img src={logo} id='banner-logo' />
+            <div id='tagline-container'>
+            <span id='tagline'>UniMelb's Premier Tech Society</span>
+            <span class='blinking-cursor'>_</span>
+            </div>
         </div>
       </div>
     </div>
