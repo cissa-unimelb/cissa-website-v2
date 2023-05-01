de1e7baf2f8337a96f89cc661c2021454d2559a1
khangluong2004 <khangluong2004@gmail.com>
2023-05-01 16:36:34 +1000

    Finish the infoCards

--- src/infoCards.jsx
+++ src/infoCards.jsx
@@ -4,7 +4,7 @@ import './assets/css/infoCard.css';
 const InfoCards = (props) => {
     return(<section class="col-md-3 my-3 cards mx-3">
           <span class="main-icon">
-            <i class="fas fa-users"></i>
+            <i class= {props.icon}></i>
           </span>
           <h4 class="mx-2">{props.title}</h4>
           <p class="px-4">{props.description}</p>
@@ -13,9 +13,9 @@ const InfoCards = (props) => {
 
 const WhatWeDo = (props) => {
     const cardDetails = [
-        {title: "Industry Connections", description: "Get to know industry professionals, gain exposure to technical talks and the chance to build career connections!"},
-        {title: "Workshops", description: "Expand your skillset and get some hands-on experience! Workshops are taught by industry and student experts!"},
-        {title: "Hackathons", description: "Come and compete for the chance to win prize money, gain some technical experience, and to put your ideas to the test!"}
+        {title: "Industry Connections", icon: "fas fa-users", description: "Get to know industry professionals, gain exposure to technical talks and the chance to build career connections!"},
+        {title: "Workshops", icon: "fas fa-book-reader", description: "Expand your skillset and get some hands-on experience! Workshops are taught by industry and student experts!"},
+        {title: "Hackathons", icon: "fas fa-code-branch", description: "Come and compete for the chance to win prize money, gain some technical experience, and to put your ideas to the test!"}
     ];
     return(<div class="section px-2 py-4 text-center">
       <h3 class="mt-5 section-title">What We Do</h3>
