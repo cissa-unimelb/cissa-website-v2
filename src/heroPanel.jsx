f15219d1ee81c1314e7f5082a1062b0e503cc4f6
khangluong2004 <khangluong2004@gmail.com>
2023-05-01 21:26:55 +1000

    Clean code, temporary add CommitteeCard

--- src/heroPanel.jsx
+++ src/heroPanel.jsx
@@ -1,38 +1,36 @@
 import './assets/css/hero.css'
 import logo from './assets/images/logo_transparent.png'
-import Typewriter from 'typewriter-effect';
+import Typewriter from 'typewriter-effect'
 
-
-
-function HeroPanel(props) {
+function HeroPanel (props) {
   return (
     <div id='banner'>
       <div id='banner-overlay'>
-        <div style={{position: 'relative', top: '35%'}}>
-            <img src={logo} id='banner-logo' />
-            <div id='tagline-container'>
+        <div style={{ position: 'relative', top: '35%' }}>
+          <img src={logo} id='banner-logo' />
+          <div id='tagline-container'>
             <span id='tagline'>
               <Typewriter
                 options={{
                   strings: [
-                    "UniMelb's Premier Tech Society", 
-                    "Bridging University and Industry",
-                    "Membership is FREE"
+                    "UniMelb's Premier Tech Society",
+                    'Bridging University and Industry',
+                    'Membership is FREE'
                   ],
                   autoStart: true,
                   loop: true,
-                  cursor: "_",
+                  cursor: '_',
                   pauseFor: 1000,
                   delay: 75,
                   deleteSpeed: 10
                 }}
               />
             </span>
-            </div>
+          </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default HeroPanel;
+export default HeroPanel
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import HeroPanel from './heroPanel';
-import Initiative from './initiative';
-import WhatWeDo from './infoCards';
+import HeroPanel from './heroPanel'
+import Initiative from './initiative'
+import WhatWeDo from './infoCards'
+import CommitteeList from './committeeCards'
 
-import './assets/css/main.css';
+import './assets/css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HeroPanel />
     <Initiative />
     <WhatWeDo />
-  </React.StrictMode>,
+    <CommitteeList />
+  </React.StrictMode>
 )
