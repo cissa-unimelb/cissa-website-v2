0735c50be532c01f5f68c9ec72f98e6ab3dd837d
khangluong2004 <khangluong2004@gmail.com>
2023-05-08 21:53:08 +1000

    Display to Cards for testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import CommitteeList from './committeeCards'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode />
-
+  <React.StrictMode>
+    <CommitteeList />
+  </React.StrictMode>
 )
