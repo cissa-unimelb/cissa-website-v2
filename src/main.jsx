f9adb78fbcaba5947d20038b5063eb1ca4886913
khangluong2004 <khangluong2004@gmail.com>
2023-05-08 21:44:15 +1000

    Move CommitteeCard to new branch

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import HeroPanel from './heroPanel'
 import Initiative from './initiative'
 import WhatWeDo from './infoCards'
-import CommitteeList from './committeeCards'
 
 import './assets/css/main.css'
 
@@ -13,6 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <HeroPanel />
     <Initiative />
     <WhatWeDo />
-    <CommitteeList />
   </React.StrictMode>
 )
