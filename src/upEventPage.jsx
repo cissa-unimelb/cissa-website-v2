fdd1b8f3ebe5743477010269bcc41af7ed895cbb
khangluong2004 <khangluong2004@gmail.com>
2023-07-03 23:00:26 +1000

    Upcoming event page completed

---
+++ src/upEventPage.jsx
@@ -0,0 +1,20 @@
+import "./assets/css/upEventPage.css";
+
+const UpEventPage = (props) => {
+    // Needs redirecting for past events
+    return (
+        <div>
+            <header className="black-header">
+                <h1 className="section-title white-header-text">EVENTS</h1>
+            </header>
+
+            <iframe className="airtable-embed" src="https://airtable.com/embed/shr9puuNFsGBcB5Wl?backgroundColor=pink"></iframe>
+            
+            <h5 className="text-center py-5">
+                Check out our past events <a href="">here</a>
+            </h5>
+        </div>
+    )
+}
+
+export default UpEventPage
