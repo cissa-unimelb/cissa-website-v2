8d21882241ca1557c96f7884e98cd4c484224508
khangluong2004 <khangluong2004@gmail.com>
2023-07-04 12:36:52 +1000

    Add images to gallery

---
+++ src/gallery.jsx
@@ -0,0 +1,39 @@
+import "./assets/css/galleryPage.css";
+
+const Gallery = (props) => {
+    return (
+        <div>
+            <header className="black-header">
+                <h1 className="section-title white-header-text">GALLERY</h1>
+            </header>
+
+            <div class="gallery-image">
+                <a href="assets/images/gallery_1.jpg" class="img-box" data-lightbox="cissapeeps" ><img class="" src="assets/images/gallery_1.jpg" alt="1" /></a>
+                <a href="assets/images/gallery_2.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_2.jpg" alt="2" /></a>
+                <a href="assets/images/gallery_3.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_3.jpg" alt="3" /></a>
+                <a href="assets/images/gallery_4.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_4.jpg" alt="4" /></a>
+                <a href="assets/images/gallery_5.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_5.jpg" alt="5"/></a>
+                <a href="assets/images/gallery_6.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_6.jpg" alt="6" /></a>
+                <a href="assets/images/gallery_7.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_7.jpg" alt="7" /></a>
+                <a href="assets/images/gallery_8.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_8.jpg" alt="8" /></a>
+                <a href="assets/images/gallery_9.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_9.jpg" alt="9" /></a>
+                <a href="assets/images/gallery_10.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_10.jpg" alt="10" /></a>
+                <a href="assets/images/gallery_11.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_11.jpg" alt="11" /></a>
+                <a href="assets/images/gallery_12.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_12.jpg" alt="12" /></a>
+                <a href="assets/images/gallery_13.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_13.jpg" alt="13" /></a>
+                <a href="assets/images/gallery_14.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_14.jpg" alt="14" /></a>
+                <a href="assets/images/gallery_15.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_15.jpg" alt="15" /></a>
+                <a href="assets/images/gallery_16.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_16.jpg" alt="16" /></a>
+                <a href="assets/images/gallery_17.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_17.jpg" alt="17" /></a>
+                <a href="assets/images/gallery_18.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_18.jpg" alt="18" /></a>
+                <a href="assets/images/gallery_20.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_20.jpg" alt="13" /></a>
+                <a href="assets/images/gallery_21.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_21.jpg" alt="14" /></a>
+                <a href="assets/images/gallery_22.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_22.jpg" alt="15" /></a>
+                <a href="assets/images/gallery_23.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_23.jpg" alt="16" /></a>
+                <a href="assets/images/gallery_24.jpg" class="img-box" data-lightbox="cissapeeps"><img class="" src="assets/images/gallery_24.jpg" alt="17" /></a>
+            </div>
+        </div>
+    )
+}
+
+export default Gallery
