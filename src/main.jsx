f16b14a2372a4332a7a45c5bba5e6f5d6e772089
khangluong2004 <khangluong2004@gmail.com>
2023-07-07 21:59:01 +1000

    Complete pastEvents page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import PastEventPage from './pastEventPage'
+import "./assets/css/main.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode />
-
+  <React.StrictMode>
+    <PastEventPage />
+  </React.StrictMode>
 )
