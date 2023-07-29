d782dc88f2ed96f1677f26b1c043af0e0b731554
khangluong2004 <khangluong2004@gmail.com>
2023-07-29 22:34:30 +1000

    Refactor for merging

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/css/main.css'
-import Gallery from './gallery'
+import Gallery from './gallery/gallery'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
