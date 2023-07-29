a21448bc1272d5a6c870693a3e480f260db96aee
khangluong2004 <khangluong2004@gmail.com>
2023-07-29 22:47:58 +1000

    Refactor before merging

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import UpEventPage from './upEventPage'
+import UpEventPage from './upEvent/upEventPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/upEvent/upEventPage.jsx
+++ src/upEvent/upEventPage.jsx
@@ -1,4 +1,4 @@
-import "./assets/css/upEventPage.css";
+import "../assets/css/upEvent/upEventPage.css";
 
 const UpEventPage = (props) => {
     // Needs redirecting for past events
