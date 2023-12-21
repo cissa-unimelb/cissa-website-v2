f769608c07da7fe9728854e59f3152a397a34702
khangluong2004 <khangluong2004@gmail.com>
2023-12-21 22:13:12 +1100

    Add line break in HeroPage

--- src/hero/infoCards.jsx
+++ src/hero/infoCards.jsx
@@ -8,6 +8,7 @@ const InfoCards = (props) => {
       </span>
       <h4 class='mx-2'>{props.title}</h4>
       <p class='px-4'>{props.description}</p>
+      <br />
     </section>
   )
 }
@@ -18,6 +19,7 @@ const WhatWeDo = (props) => {
     { title: 'Workshops', icon: 'fas fa-book-reader', description: 'Expand your skillset and get some hands-on experience! Workshops are taught by industry and student experts!' },
     { title: 'Hackathons', icon: 'fas fa-code-branch', description: 'Come and compete for the chance to win prize money, gain some technical experience, and to put your ideas to the test!' }
   ]
+  
   return (
     <div class='section px-2 py-4 text-center'>
       <h3 class='mt-5 section-title'>What We Do</h3>
