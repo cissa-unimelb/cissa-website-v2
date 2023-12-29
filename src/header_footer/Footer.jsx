8bd1d44d98206e9e87d2adcabe0c671a12b27507
Michael Ren <michaelren0129@gmail.com>
2023-12-29 16:49:34 +0800

    Close issue VM-25
    - Added SM links
    - Updated Twitter icon to X

--- src/header_footer/Footer.jsx
+++ src/header_footer/Footer.jsx
@@ -1,24 +1,22 @@
-import { GrMedium } from 'react-icons/gr'
 import { ImFacebook } from 'react-icons/im'
 import { AiOutlineInstagram } from 'react-icons/ai'
-import { FaTwitter, FaDiscord } from 'react-icons/fa'
+import { FaMediumM, FaDiscord } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
 import Icon from './Icon'
 
-function Footer () {
+export default function Footer () {
   return (
     <footer>
       <div className='footer__content'>
         <div className='icons'>
-          <Icon icon={GrMedium} />
-          <Icon icon={ImFacebook} />
-          <Icon icon={AiOutlineInstagram} />
-          <Icon icon={FaTwitter} />
-          <Icon icon={FaDiscord} />
+          <a href="https://medium.com/cissa-unimelb" target="_blank"><Icon icon={FaMediumM} /></a>
+          <a href="https://www.facebook.com/cissa.unimelb/" target="_blank"><Icon icon={ImFacebook} /></a>
+          <a href="https://www.instagram.com/cissa_unimelb/" target="_blank"><Icon icon={AiOutlineInstagram} /></a>
+          <a href="https://twitter.com/CISSA_UniMelb" target="_blank"><Icon icon={FaXTwitter} /></a>
+          <a href="https://discord.com/invite/Dq9A5GFFBD" target="_blank"><Icon icon={FaDiscord} /></a>
         </div>
         <p className='copyright'>Copyright © 2023 CISSA</p>
       </div>
     </footer>
   )
 }
-
-export default Footer
