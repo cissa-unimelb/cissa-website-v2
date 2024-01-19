6d47793a0c97cfbaaa55ff0d561e58c9992592ab
Michael Ren <michaelren0129@gmail.com>
2024-01-19 20:03:08 +0800

    Fix VM-29, VM-39

--- src/hero/initiative.jsx
+++ src/hero/initiative.jsx
@@ -3,16 +3,16 @@ import cissa_all from '../assets/images/cissa_all.jpg'
 
 function Initiative (props) {
   return (
-    <div className='row section semi-dark our-initiative py-5 d-flex justify-content-center'>
-      <div className='col-lg-5'>
-        <h3 className='white-text my-3 section-title text-center'>Our Initiative </h3>
-        <p class='cissa-intro mt-5'>
-          We hold social, industry and networking events regularly. Whether you are looking
-          for academic courses or searching for work opportunities in different IT-related areas, CISSA is the place to go.
-          If you are more of a hands-on type of person, you will enjoy Codebrew, our annual hackathon. <a href='events.html' class='dark-link'>Check out our upcoming events!</a>
-        </p>
-      </div>
-      <div className='col-lg-5 py-2 mt-5 justify-content-center'>
+    <div className='section semi-dark our-initiative justify-content-center'>
+      <div className="grid">
+        <div>
+          <h3 className='white-text my-3 section-title text-center'>Our Initiative </h3>
+          <p class='cissa-intro mt-5'>
+            We hold social, industry and networking events regularly. Whether you are looking
+            for academic courses or searching for work opportunities in different IT-related areas, CISSA is the place to go.
+            If you are more of a hands-on type of person, you will enjoy Codebrew, our annual hackathon. <a href='events.html' class='dark-link'>Check out our upcoming events!</a>
+          </p>
+        </div>
         <img className='img-about' src={cissa_all} alt='Photo' />
       </div>
     </div>
--- src/hero/uniAffiliation.jsx
+++ src/hero/uniAffiliation.jsx
@@ -3,9 +3,8 @@ import logo from "../assets/images/UMSU-Affiliated.png"
 
 function UniAfflict (props) {
   return (
-    <div class="section px-2 py-4 text-center">
-        <h3 class="mt-5 section-title">University Affiliations</h3>
-        <br/>
+    <div class="section px-4 text-center affiliation">
+        <h3 class="section-title">University Affiliations</h3>
         <img src={logo} class="logo"></img>
     </div>
     )
