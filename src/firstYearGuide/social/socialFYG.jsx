e75bdb7e5c56f5cd336b4e517c662cb6c5719e1d
khangluong2004 <khangluong2004@gmail.com>
2024-02-11 17:01:21 +1100

    Add anchor

--- src/firstYearGuide/social/socialFYG.jsx
+++ src/firstYearGuide/social/socialFYG.jsx
@@ -2,16 +2,17 @@ import '../assets/css/socialFYG.css'
 import mobile_Alan_Gilbert from "../assets/images/mobile-Alan-Gilbert.png"
 import mobile_Alan_Gilbert2x from "../assets/images/mobile-Alan-Gilbert@2x.png"
 
-import { useEffect } from 'react';
-import {} from '../components/popup/popupButton.jsx';
+import { useEffect, useRef, useState} from 'react';
+import PopupButton from '../components/popup/popupButton.jsx';
 
 const socialFYG = (props) => {
     const div = useRef(null);
-    const [baseAnchor, setAnchor] = useState({});
+    const [baseAnchor, setAnchor] = useState({x: 0, y: 0, width: 0, height: 0});
 
     useEffect(() => {
         const rect = div.current.getBoundingClientRect();
-        const { x, y, width, height } = rect;
+        let { x, y, width, height } = rect;
+        x = x % (window.innerWidth);
         setAnchor({ x, y, width, height });
     }, []);
 
@@ -19,7 +20,8 @@ const socialFYG = (props) => {
     return (
         <div className='socialContainer'>
             <img 
-                src={mobile_Alan_Gilbert} 
+                src={mobile_Alan_Gilbert}
+                ref={div} 
                 alt="Alan Gilbert"
                 style={{
                     marginBottom: "10%",
@@ -29,6 +31,43 @@ const socialFYG = (props) => {
                     bottom: 0,
                 }} 
             />
+
+            <PopupButton 
+                text="Making friends" 
+                speechBubble={{
+                    anchor: [baseAnchor.x + baseAnchor.width * 0.5, baseAnchor.y + baseAnchor.height * 0.15],
+                    maxWidth: 200,
+                    bound: false
+                }}
+            >
+                <h3>Where is the quick brown fox?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </PopupButton>
+
+            <PopupButton 
+                text="Group mates" 
+                speechBubble={{
+                    anchor: [baseAnchor.x + baseAnchor.width * 0.6, baseAnchor.y + baseAnchor.height * 0.5],
+                    maxWidth: 200,
+                    //bound: false
+                }}
+            >
+                <h3>Where is the quick brown fox?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </PopupButton>
+
+            <PopupButton 
+                text="Event list" 
+                speechBubble={{
+                    anchor: [baseAnchor.x + baseAnchor.width * 0.1, baseAnchor.y + baseAnchor.height * 0.8],
+                    maxWidth: 200,
+                    //bound: false
+                }}
+            >
+                <h3>Where is the quick brown fox?</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </PopupButton>
+
         </div>
     )
 }
