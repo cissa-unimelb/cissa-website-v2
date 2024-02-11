af112322558b69048e4a44203c271d8af437b999
khangluong2004 <khangluong2004@gmail.com>
2024-02-11 15:56:05 +1100

    Adding anchor

--- src/firstYearGuide/social/socialFYG.jsx
+++ src/firstYearGuide/social/socialFYG.jsx
@@ -1,8 +1,21 @@
-import '../assets/css/socialFYG.css'
+import { useEffect } from 'react';
+import {} from '../components/popup/popupButton.jsx';
+import '../assets/css/socialFYG.css';
 
 const socialFYG = (props) => {
+    const div = useRef(null);
+    const [baseAnchor, setAnchor] = useState({});
+
+    useEffect(() => {
+        const rect = div.current.getBoundingClientRect();
+        const { x, y, width, height } = rect;
+        setAnchor({ x, y, width, height });
+    }, []);
+
+
     return (
-        <div>
+        <div className='socialContainer'>
+            
         </div>
     )
 }
