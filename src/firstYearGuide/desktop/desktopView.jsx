537395b938dc60b2b1ff084ee6dd7608442de769
khangluong2004 <khangluong2004@gmail.com>
2024-02-15 22:07:45 +1100

    Finish set up the desktop

--- src/firstYearGuide/desktop/desktopView.jsx
+++ src/firstYearGuide/desktop/desktopView.jsx
@@ -4,17 +4,47 @@ import Road from "../components/road/road";
 import Stars from "../components/stars/stars"
 import Moon from "../components/moon/moon";
 
+import {useRef, useState} from 'react'
+
 const DesktopView = (props) => {
+    const buildingImg = useRef(null);
+    const [baseAnchor, setAnchor] = useState({x: 0, y: 0, width: 0, height: 0});
+
+    const callback = () => {
+        const EPSILON = 2;
+
+        const rect = buildingImg.current.getBoundingClientRect()
+        let { x, y, width, height } = rect
+        x = (Math.ceil(x + window.globalScroll)) % window.innerWidth
+        y = y % window.innerHeight
+
+        // For landscape object with overflow section (image not fit in frame)
+        // or image spanning entire width
+        if (width > window.innerWidth || Math.abs(width - window.innerWidth) < EPSILON){
+            x = 0;
+            width = window.innerWidth;
+        }
+
+        setAnchor({ x, y, width, height })
+        console.log(x, y, width, height)
+        // console.log(window.globalScroll)
+        // console.log(x)
+        // console.log(rect)
+    }
+
     return(
         <div className="desktopView">
             <div className="desktopView__buildingWrapper">
-                <img 
+                <img
+                    ref={buildingImg} 
                     src={buildingCluster}
                     style={{
                         width: "100vw",
                         maxWidth: '1100px',
                         zIndex: 3
                     }}
+
+                    onLoad={callback}
                 />
             </div>
             
